f879928b0122993f0977cdc72e87bf0a91e93c92
snapshotpl <witold@wasiczko.pl>
2015-09-01 22:48:03 +0200

    Tests

--- test/RequestIdMiddlewareTest.php
+++ test/RequestIdMiddlewareTest.php
@@ -16,7 +16,7 @@ class RequestIdMiddlewareTest extends \PHPUnit_Framework_TestCase
         $this->generator = $this->getMock(GeneratorInterface::class);
     }
 
-    public function testLogRequest()
+    public function testGenerateIdAndEmmitToResponse()
     {
         $this->generator->expects($this->once())->method('generateRequestId')->willReturn('123456789');
 
@@ -36,5 +36,52 @@ class RequestIdMiddlewareTest extends \PHPUnit_Framework_TestCase
         $this->assertTrue($calledOut, 'Out is not called');
         $this->assertNotSame($response, $result);
         $this->assertEquals('123456789', $result->getHeaderLine(RequestIdMiddleware::HEADER_REQUEST_ID));
+    }
+
+    public function testGenerateIdAndNotEmmitToResponse()
+    {
+        $this->generator->expects($this->once())->method('generateRequestId')->willReturn('123456789');
+
+        $requestId = new RequestIdMiddleware($this->generator, true, false);
+        $request = new ServerRequest();
+        $response = new Response();
+        $calledOut = false;
+
+        $outFunction = function ($request, $response) use (&$calledOut) {
+            $calledOut = true;
+
+            return $response;
+        };
+
+        $result = call_user_func($requestId, $request, $response, $outFunction);
+
+        $this->assertTrue($calledOut, 'Out is not called');
+        $this->assertSame($response, $result);
+        $this->assertEquals(null, $result->getHeaderLine(RequestIdMiddleware::HEADER_REQUEST_ID));
+    }
+
+    /**
+     * @dataProvider dataproviderEmptyRequestIdValues
+     * @expectedException PhpMiddleware\RequestId\Exception\NotGenerated
+     */
+    public function testTryToGenerateEmptyRequestId($emptyValue)
+    {
+        $this->generator->expects($this->once())->method('generateRequestId')->willReturn($emptyValue);
+
+        $requestId = new RequestIdMiddleware($this->generator);
+        $request = new ServerRequest();
+        $response = new Response();
+
+        $outFunction = function () {};
+
+        call_user_func($requestId, $request, $response, $outFunction);
+    }
+
+    public function dataproviderEmptyRequestIdValues()
+    {
+        return [
+            [''],
+            [null],
+        ];
     }
 }
