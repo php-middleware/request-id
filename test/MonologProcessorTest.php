2837632fc46eb71cfd3f93ff608c8f8251ad90c1
Tomek Marcinkowski <tomasz.marcinkowski@rst.com.pl>
2016-03-23 16:22:25 +0100

    FIX: MonologProcessor handles particular kind of exceptions thrown from RequestIdProviders

--- test/MonologProcessorTest.php
+++ test/MonologProcessorTest.php
@@ -2,28 +2,43 @@
 
 namespace PhpMiddlewareTestTest\RequestId;
 
+use PhpMiddleware\RequestId\Exception\MissingRequestId;
 use PhpMiddleware\RequestId\MonologProcessor;
 use PhpMiddleware\RequestId\RequestIdProviderInterface;
 
 class MonologProcessorTest extends \PHPUnit_Framework_TestCase
 {
     protected $processor;
-
-    protected function setUp()
-    {
-        $requestIdProvider = $this->getMock(RequestIdProviderInterface::class);
-        $requestIdProvider->expects($this->once())->method('getRequestId')->willReturn('boo');
-
-        $this->processor = new MonologProcessor($requestIdProvider);
-    }
+    /**
+     * @var RequestIdProviderInterface|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $requestIdProvider;
 
     public function testIsRequestIdInRecord()
     {
+        $this->requestIdProvider->expects($this->once())->method('getRequestId')->willReturn('boo');
         $record = ['extra' => []];
 
         $newRecord = call_user_func($this->processor, $record);
 
         $this->assertArrayHasKey(MonologProcessor::KEY, $newRecord['extra']);
         $this->assertSame('boo', $newRecord['extra'][MonologProcessor::KEY]);
+    }
+
+    public function testIsMissingRequestIdExceptionHandledProperly()
+    {
+        $this->requestIdProvider->expects($this->once())->method('getRequestId')->willThrowException(new MissingRequestId());
+        $record = ['extra' => []];
+
+        $newRecord = call_user_func($this->processor, $record);
+
+        $this->assertArrayHasKey(MonologProcessor::KEY, $newRecord['extra']);
+        $this->assertNull($newRecord['extra'][MonologProcessor::KEY]);
+    }
+
+    protected function setUp()
+    {
+        $this->requestIdProvider = $this->getMock(RequestIdProviderInterface::class);
+        $this->processor         = new MonologProcessor($this->requestIdProvider);
     }
 }
